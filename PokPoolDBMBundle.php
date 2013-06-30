be0c64dc7b2550edd90038af17edf0dc72950028
Florent Denis <florentdenisp@gmail.com>
2013-06-30 23:30:23 +0200

    Improve bundle boot

--- PokPoolDBMBundle.php
+++ PokPoolDBMBundle.php
@@ -2,6 +2,7 @@
 
 namespace Pok\Bundle\PoolDBMBundle;
 
+use Symfony\Bundle\FrameworkBundle\Console\Application as FrameworkConsoleApplication;
 use Symfony\Component\Console\Application;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -14,8 +15,17 @@ class PokPoolDBMBundle extends Bundle
      */
     public function registerCommands(Application $application)
     {
+        if (!$application instanceof FrameworkConsoleApplication) {
+            return;
+        }
+
 //        parent::registerCommands($application);
 
+        $application->setHelperSet(ConsoleRunner::createHelperSet(
+            $this->container->get('pok.pool_dbm.manager'),
+            null,
+            $application->getKernel()->getCacheDir()
+        ));
         ConsoleRunner::addDefaultCommands($application);
     }
 }
